60f609545b3935c178b0a5674a37cf3337561cdf
Aman Verma <avman1998@gmail.com>
2023-08-16 16:31:59 +0530

    cart functionality

--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
 import { products } from "./config";
 import Product from "./Product";
 const MyPage = () => {
-  console.log(products);
+  const [cart, setCart] = useState([]);
   const listedProducts = products.map((item, index) => {
-    return <Product key={index} image={item.image} title={item.title} />;
+    return <Product key={index} item={item} setCart={setCart} />;
   });
 
   return (
     <div className="flex flex-col gap-2">
       <div className="flex gap-2">{listedProducts}</div>
-      <h1>Cart: 0</h1>
+      <h1>Cart: {cart.length}</h1>
     </div>
   );
 };
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,9 +1,21 @@
 const Product = (prop) => {
+  console.log("This is my setCart function ", prop.setCart);
+  function addtoCart(item) {
+    prop.setCart((prev) => {
+      return [...prev, item];
+    });
+  }
+
   return (
     <div>
-      <img src={prop.image} alt={prop.title} width="200px" height="200px" />
-      <p>{prop.title}</p>
-      <button>Add to cart</button>
+      <img
+        src={prop.item.image}
+        alt={prop.item.title}
+        width="200px"
+        height="200px"
+      />
+      <p>{prop.item.title}</p>
+      <button onClick={() => addtoCart(prop.item)}>Add to cart</button>
     </div>
   );
 };
